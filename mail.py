abde004cf929d936a703ac868b012f46e0d10228
SmitJethwa <smitjethwa20@gmail.com>
2023-07-24 11:20:51 +0530

    env added

--- mail.py
+++ mail.py
@@ -5,6 +5,20 @@ from email.mime.base import MIMEBase
 from email import encoders 
 import pandas as pd
 import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+
+SMTP_URL = os.environ.get("SMTP_URL")
+PORT = os.environ.get("PORT")
+SENDER_NAME = os.environ.get("SENDER_NAME")
+SENDER = os.environ.get("SENDER")
+PASSWORD = os.environ.get("PASSWORD")
+
+
 
 df = pd.read_csv('list.csv')   # Read CSV file
 products_list = df.values.tolist()
@@ -14,22 +28,17 @@ for i in products_list:
     receiver_list.append(i[1])
     name_list.append(i[0])
 
-sender = ""  # Sender's Email Address.
-
-password = "" # Password here
-
-# print(sender, password)
 
 def send_mail(i):    
     msg = MIMEMultipart()       # instance of MIMEMultipart   
-    msg['From'] = sender        # storing the senders email address   
+    msg['From'] = f"{SENDER_NAME}  {SENDER}"        # storing the senders email address   
     msg['To'] = receiver_list[i]         # storing the receivers email address  
     # print(receiver_list[i])
-    msg['Subject'] = "" # storing the subject  
+    msg['Subject'] = "Hello WOrld" # storing the subject  
     # string to store the body of the mail 
     body = f''' 
     # Mail body here. 
-    # Varibales can be accessed here for e.g. 
+    # Variables can be accessed here for e.g. 
     Hi {name_list[i]},
     
     This is a test mail.
@@ -39,7 +48,8 @@ def send_mail(i):
     
     '''
     msg.attach(MIMEText(body, 'plain'))     # attach the body with the msg instance 
-    pdf_name = f'{name_list[i]}_{i}.pdf'        # open the file to be sent  
+    pdf_name = 'test.pdf'
+    # pdf_name = f'{name_list[i]}_{i}.pdf'        # open the file to be sent  
     filename = pdf_name
     attachment = open(filename, "rb") 
     p = MIMEBase('application', 'octet-stream')      # instance of MIMEBase and named as p 
@@ -47,17 +57,17 @@ def send_mail(i):
     encoders.encode_base64(p)       # encode into base64  
     p.add_header('Content-Disposition', "attachment; filename= %s" % filename) 
     msg.attach(p)   # attach the instance 'p' to instance 'msg' 
-    s = smtplib.SMTP('smtp.gmail.com', 587)  # creates SMTP session 
+    s = smtplib.SMTP(SMTP_URL, PORT)  # creates SMTP session 
     s.starttls() # start TLS for security
-    s.login(sender, password)  # Authentication 
+    s.login(SENDER,PASSWORD)  # Authentication 
     text = msg.as_string() # Converts the Multipart msg into a string 
-    s.sendmail(sender, receiver_list[i], text)  # sending the mail 
+    s.sendmail(SENDER, receiver_list[i], text)  # sending the mail 
     print(f'{i+1} ==> Mail sent to {name_list[i]} ==> {receiver_list[i]}')
     s.quit() # terminating the session 
 
 
 def rename_pdf():
-  '''Rename pdf file. '''
+    '''Rename pdf file. '''
     os.chdir('C:\\Users\\HP\\Documents\\Python Scripts\\mail\\data')
     i=0
     for file in os.listdir():
@@ -67,7 +77,7 @@ def rename_pdf():
         print(f'File renamed with {dst}')
         i+=1
 
-rename_pdf()  # Call this function if you wants to change pdf name.
+# rename_pdf()  # Call this function if you wants to change pdf name.
 
 for i in range(len(receiver_list)):
     send_mail(i)
